273b94ae8eb4c4506072a2fcb0f9e8b0232e9573
AungThanSoe <akyab.aungthansoe@gmail.com>
2020-10-04 18:25:14 +0630

    added Sign Form

---
+++ src/pages/sign-in-and-sign-up/signin-and-signup.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './signin-and-signup.component.styles.scss';
+
+export const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+)
