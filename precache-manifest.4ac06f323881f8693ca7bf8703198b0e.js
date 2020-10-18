9fd6f0a351e794a6a61149c04944c96f6dacd32f
AungThanSoe <akyab.aungthansoe@gmail.com>
2020-10-18 22:29:04 +0630

    Updates

---
+++ precache-manifest.4ac06f323881f8693ca7bf8703198b0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fc6ebd61c7c80d6c91736e3be45bc8",
+    "url": "/aung-than-soe/crown-clothing/index.html"
+  },
+  {
+    "revision": "1ad983245cdca3fed34e",
+    "url": "/aung-than-soe/crown-clothing/static/css/main.f50947f9.chunk.css"
+  },
+  {
+    "revision": "2c2c557e605040dd1d6d",
+    "url": "/aung-than-soe/crown-clothing/static/js/2.d047c72f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/aung-than-soe/crown-clothing/static/js/2.d047c72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad983245cdca3fed34e",
+    "url": "/aung-than-soe/crown-clothing/static/js/main.fc7590fd.chunk.js"
+  },
+  {
+    "revision": "92e52060e8745461f449",
+    "url": "/aung-than-soe/crown-clothing/static/js/runtime-main.14163124.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/aung-than-soe/crown-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/aung-than-soe/crown-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
