f89b1ff14051417ae6841c050422eaa9b2dcbe1b
AungThanSoe <akyab.aungthansoe@gmail.com>
2020-10-18 22:50:48 +0630

    changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { persistor, store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
