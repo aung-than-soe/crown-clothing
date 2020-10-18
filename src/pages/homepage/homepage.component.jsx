b6c6955a87ed2103207a2b0366dcb734412e2bdc
AungThanSoe <akyab.aungthansoe@gmail.com>
2020-10-18 20:28:53 +0630

    added collection depend on collectionId route

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Directory } from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component'
 import './homepage.style.scss';
 
 export const HomePage = () => (
     <div className="homepage">
-        <Directory/>
+        <Directory />
     </div>
 )
